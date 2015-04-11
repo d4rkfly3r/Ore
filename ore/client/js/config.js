35a1b61b5fa2d6325c423371b1905a019f721b22
Luke Granger-Brown <git@lukegb.com>
2015-04-12 00:21:39 +0100

    Reconfigure Angular's CSRF stuff for Django

--- ore/client/js/config.js
+++ ore/client/js/config.js
@@ -2,3 +2,7 @@ angular.module('ore')
     .config(function (RestangularProvider) {
         RestangularProvider.setRequestSuffix('/');
     })
+    .config(function($httpProvider) {
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    });
