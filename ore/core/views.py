bff1b20c5b5c2aa314e234e5c45fd92c3fb96b39
Luke Granger-Brown <git@lukegb.com>
2015-04-12 00:25:05 +0100

    Adds NamespaceViewSet back, whoops

--- ore/core/views.py
+++ ore/core/views.py
@@ -1,5 +1,18 @@
 from django.views.generic.base import TemplateView
+from rest_framework.viewsets import ModelViewSet
+
+from .serializers import NamespaceSerializer
+from .models import Namespace
 
 
 class AppView(TemplateView):
+
     template_name = 'app.html'
+
+
+class NamespaceViewSet(ModelViewSet):
+
+    serializer_class = NamespaceSerializer
+
+    def get_queryset(self):
+        return Namespace.objects.as_user(self.request.user).all()
